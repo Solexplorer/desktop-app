b0222bf04278969fd1e0af536c11232abed5be64
Solexplorer <solexplorer.noreply@github.com>
2020-04-18 09:30:04 +0200

    Add correct desktop app and MSI

--- buildWIN32.sh
+++ buildWIN32.sh
@@ -1,2 +1,2 @@
-electron-packager . gitcoin --overwrite --asar=true --platform=win32 --arch=ia32 --icon Icon/Icon.icns --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="Gitcoin"
+electron-packager . gitcoin --overwrite --platform=win32 --arch=x64 --icon Icon/Icon.icns --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName="Gitcoin"
 
---
+++ gitcoin-win32-x64/resources/app/README.md
@@ -0,0 +1,13 @@
+# Gitcoin Desktop app
+
+This is a fork of https://github.com/electron/electron-quick-start but for gitcoin.co/chat
+
+Run this command to build the desktop app.
+
+```bash
+sh build.sh
+```
+
+## License
+
+[CC0 1.0 (Public Domain)](LICENSE.md)
